137cc546448759a7bddad936359067ad696ff3a0
hoogan98 <41339852+hoogan98@users.noreply.github.com>
2019-06-07 10:56:11 -0400

    so it begins

--- gui.ps1
+++ gui.ps1
@@ -1,8 +1,9 @@
-echo "nyello"
+	
 while (1) {
 	echo "loop"
 	Start-Sleep -Seconds 1
 	$turn = Get-Content -Path .\turn.txt
+	echo $turn
 	if ($turn -eq "stop") {
 		Clear-Content -Path .\turn.txt
 		Add-Content -Path .\turn.txt -Value "start"
