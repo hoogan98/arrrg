7f9aefadc864625e668d9755ac174580e80dcd2e
hoogan98 <41339852+hoogan98@users.noreply.github.com>
2019-06-07 14:06:36 -0400

    prints ship in technicolor©

--- gui.ps1
+++ gui.ps1
@@ -1,13 +1,17 @@
 #	SHIP PRINTING
-Get-Content .\defaultShip.txt | ForEach-Object {
-    echo $_
+Get-Content .\images\defaultShip.txt | ForEach-Object {
+    $b = $_.Substring(0,30)
+	$m = $_.Substring(30,20)
+	$s = $_.Substring(50)
+	Write-Host -NoNewline -F "red" $b
+	Write-Host -NoNewline $m
+	Write-Host -F "blue" $s
 }
 $turn = Get-Content -Path .\turn.txt
 echo $turn
 
 #	CHECK FOR CLOSE
 while (1) {
-	echo "loop"
 	Start-Sleep -Seconds 1
 	$turn = Get-Content -Path .\turn.txt
 	if ($turn -eq "stop") {
