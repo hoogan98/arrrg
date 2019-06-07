dc5aaccb4b8bd52fd34940e0b6997f7c3111c8eb
hoogan98 <41339852+hoogan98@users.noreply.github.com>
2019-06-07 11:54:14 -0400

    this is gonna make a lot of files
    i thought printing the ship was gonna be the easy part reeeeee

--- gui.ps1
+++ gui.ps1
@@ -1,9 +1,15 @@
-	
+#	SHIP PRINTING
+Get-Content .\defaultShip.txt | ForEach-Object {
+    echo $_
+}
+$turn = Get-Content -Path .\turn.txt
+echo $turn
+
+#	CHECK FOR CLOSE
 while (1) {
 	echo "loop"
 	Start-Sleep -Seconds 1
 	$turn = Get-Content -Path .\turn.txt
-	echo $turn
 	if ($turn -eq "stop") {
 		Clear-Content -Path .\turn.txt
 		Add-Content -Path .\turn.txt -Value "start"
