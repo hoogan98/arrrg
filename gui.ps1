b69152e4c3beb90186300d7aa5c1702c490056a4
hoogan98 <41339852+hoogan98@users.noreply.github.com>
2019-06-07 14:39:02 -0400

    color changes with ship damage

--- gui.ps1
+++ gui.ps1
@@ -1,14 +1,36 @@
 #	SHIP PRINTING
+$turn = Get-Content -Path .\turn.txt
+echo $turn
+$health = $turn.Split(",")
+$bc = "white"
+$mc = "white"
+$sc = "white"
+if ([int]$health[3] -lt 41) {
+	$bc = "red"
+} elseif ([int]$health[3] -lt 71) {
+	$bc = "Yellow"
+} 
+if ([int]$health[7] -lt 41) {
+	$mc = "red"
+} elseif ([int]$health[7] -lt 71) {
+	$mc = "yellow"
+}
+if ([int]$health[11] -lt 41) {
+	$sc = "red"
+} elseif ([int]$health[11] -lt 71) {
+	$sc = "Yellow"
+}
+
 Get-Content .\images\defaultShip.txt | ForEach-Object {
     $b = $_.Substring(0,30)
 	$m = $_.Substring(30,20)
 	$s = $_.Substring(50)
-	Write-Host -NoNewline -F "red" $b
-	Write-Host -NoNewline $m
-	Write-Host -F "blue" $s
+	
+	
+	Write-Host -NoNewline -F $bc $b
+	Write-Host -NoNewline -F $mc $m
+	Write-Host -F $sc $s
 }
-$turn = Get-Content -Path .\turn.txt
-echo $turn
 
 #	CHECK FOR CLOSE
 while (1) {
