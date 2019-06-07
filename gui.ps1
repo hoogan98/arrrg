ad76f711e7209ae3607350187197dbdcbd7a2ed4
hoogan98 <41339852+hoogan98@users.noreply.github.com>
2019-06-07 10:15:37 -0400

    i guess it exists now

--- gui.ps1
+++ gui.ps1
@@ -1,1 +1,13 @@
-
+echo "nyello"
+while (1) {
+	echo "loop"
+	Start-Sleep -Seconds 1
+	$turn = Get-Content -Path .\turn.txt
+	if ($turn -eq "stop") {
+		Clear-Content -Path .\turn.txt
+		Add-Content -Path .\turn.txt -Value "start"
+		echo "stop found"
+		Start-Sleep -Seconds 1
+		exit
+	}
+}
