0bd32e42e1c8e04b0837511078edcfd75242100c
hoogan98 <41339852+hoogan98@users.noreply.github.com>
2019-06-21 10:34:28 -0400

    added helpful help functions

--- ships.ps1
+++ ships.ps1
@@ -172,8 +172,27 @@ function makeRegularShip() {
 			return $Dis
 		}
 		
+		function reference() {
+			write-host "choose from: 'grape', 'chain', 'round', 'wait', 'board', 'move', 'retreat', 'repair', 'rearm', 'flame', 'brace', 'sail', or 'help'"
+		}
+		
 		function help() {
-			echo "choose from: 'grape', 'chain', 'round', 'wait', 'board', 'move', 'retreat', 'repair', 'rearm', 'flame', 'brace', 'sail', or 'help'"
+			write-host "Standard Ship:"
+			write-host "The average all-rounder ship. Comes with no particular strengths or weaknesses." 
+			write-host "Actions:"
+			write-host "Grape: fire grapeshot. Damage: --crew, /\cannons, \/hull"
+			write-host "Chain: fire chainshot. Damage: /\crew, \/cannons, --hull"
+			write-host "Round: fire roundshot. Damage: \/crew, /\cannons, --hull"
+			write-host "Wait: skip an action"
+			write-host "Board: board the enemy ship with your crew"
+			write-host "Move: move your crew from one zone to another on a ship"
+			write-host "Retreat: pull back boarders from the opponent's ship"
+			write-host "Repair: order the crew to patch the hull in a zone"
+			write-host "Rearm: order the crew to move cannons from one zone to another"
+			write-host "Flame: order the crew to set fire to a zone"
+			write-host "Brace: reduce incoming damage to a zone on the next turn"
+			write-host "Sail: change the distance between the ships"
+			write-host "Reference: get a quick list of commands to run in case you forget syntax"
 		}
 
         Export-ModuleMember -Variable Name
@@ -196,6 +215,7 @@ function makeRegularShip() {
 		Export-ModuleMember -Function sail
 		Export-ModuleMember -Function help
 		Export-ModuleMember -Function reArm
+		Export-ModuleMember -Function reference
     }
     return $ship
 }
@@ -356,8 +376,30 @@ function makeRammingShip() {
 			return $Dis
 		}
 		
-		function help() {
+		function reference() {
 			write-host "choose from: 'grape', 'chain', 'round', 'wait', 'board', 'move', 'retreat', 'repair', 'rearm', 'flame', 'brace', 'sail', 'ram', or 'help'"
+		}
+		
+		function help() {
+			write-host "Ramming Ship:"
+			write-host "The bow has been modified to ram enemy ships, with a thicker hull but no cannon ports and less room for crew"
+			write-host "If you are at distance 1, you can ram the enemy for massive hull damage"
+			write-host "Notice the mid and stern hulls are weaker, but contain an impressive number of cannon ports"
+			write-host "Actions:"
+			write-host "Grape: fire grapeshot. Damage: --crew, /\cannons, \/hull"
+			write-host "Chain: fire chainshot. Damage: /\crew, \/cannons, --hull"
+			write-host "Round: fire roundshot. Damage: \/crew, /\cannons, --hull"
+			write-host "Wait: skip an action"
+			write-host "Board: board the enemy ship with your crew"
+			write-host "Move: move your crew from one zone to another on a ship"
+			write-host "Retreat: pull back boarders from the opponent's ship"
+			write-host "Repair: order the crew to patch the hull in a zone"
+			write-host "Rearm: order the crew to move cannons from one zone to another"
+			write-host "Flame: order the crew to set fire to a zone"
+			write-host "Brace: reduce incoming damage to a zone on the next turn"
+			write-host "Sail: change the distance between the ships"
+			write-host "Reference: get a quick list of commands to run in case you forget syntax"
+			write-host "Ram: move from distance 1 to 0, charging the enemy ship and dealing high hull damage to one zone"
 		}
 		
 		function dmgRam() {
@@ -400,6 +442,7 @@ function makeRammingShip() {
 		Export-ModuleMember -Function help
 		Export-ModuleMember -Function dmgRam
 		Export-ModuleMember -Function reArm
+		Export-ModuleMember -Function reference
     }
     return $ship
 }
@@ -544,8 +587,28 @@ function makeUndeadShip() {
 			return $Dis
 		}
 		
+		function reference() {
+			write-host "choose from: 'grape', 'chain', 'round', 'wait', 'board', 'move', 'retreat', 'repair', 'rearm', 'flame', 'brace', 'sail', 'resurrect', or 'help'"
+		}
+		
 		function help() {
-			echo "choose from: 'grape', 'chain', 'round', 'wait', 'board', 'move', 'retreat', 'repair', 'rearm', 'flame', 'brace', 'sail', 'resurrect', or 'help'"
+			write-host "Undead Ship:"
+			write-host "The crew of this vessel are pulled straight from the bottom of the sea, and are expendable"
+			write-host "Unfortunately their life force is tied to a cursed amulet on the ship, meaning they can never leave"
+			write-host "If you begin to take casualties, you can 'ressurect' your crew to call them back from the depths"
+			write-host "Actions:"
+			write-host "Grape: fire grapeshot. Damage: --crew, /\cannons, \/hull"
+			write-host "Chain: fire chainshot. Damage: /\crew, \/cannons, --hull"
+			write-host "Round: fire roundshot. Damage: \/crew, /\cannons, --hull"
+			write-host "Wait: skip an action"
+			write-host "Move: move your crew from one zone to another on a ship"
+			write-host "Repair: order the crew to patch the hull in a zone"
+			write-host "Rearm: order the crew to move cannons from one zone to another"
+			write-host "Flame: order the crew to set fire to a zone"
+			write-host "Brace: reduce incoming damage to a zone on the next turn"
+			write-host "Sail: change the distance between the ships"
+			write-host "Reference: get a quick list of commands to run in case you forget syntax"
+			write-host "Ressurect: revive some of the dead crew, works best if the crew is around half strength"
 		}
 		
 		function resurrect($zone) {
@@ -578,6 +641,7 @@ function makeUndeadShip() {
 		Export-ModuleMember -Function help
 		Export-ModuleMember -Function reArm
 		Export-ModuleMember -Function resurrect
+		Export-ModuleMember -Function reference
     }
     return $ship
 }
@@ -701,8 +765,28 @@ function makeVikingShip() {
 			return $Dis
 		}
 		
+		function reference() {
+			write-host "choose from: 'wait', 'board', 'move', 'retreat', 'repair', 'flame', 'brace', 'sail', 'arrows', or 'help'"
+		}
+		
 		function help() {
-			echo "choose from: 'wait', 'board', 'move', 'retreat', 'repair', 'flame', 'brace', 'sail', 'arrows', or 'help'"
+			write-host "Viking Ship:"
+			write-host "I know these guys are from a different time but they're just so cool I couldn't resist"
+			write-host "The Viking longship is not nearly as heavily armored as other ships, watch your hull health"
+			write-host "The Vikings do not have cannons, but they have bows that can be fired from distance 2 or closer"
+			write-host "Viking crew members are a formidable force, dealing high damage to both crew and structures with above-average defense"
+			write-host "Actions:"
+			write-host "Wait: skip an action"
+			write-host "Board: board the enemy ship with your crew"
+			write-host "Move: move your crew from one zone to another on a ship"
+			write-host "Retreat: pull back boarders from the opponent's ship"
+			write-host "Repair: order the crew to patch the hull in a zone"
+			write-host "Rearm: order the crew to move cannons from one zone to another"
+			write-host "Flame: order the crew to set fire to a zone"
+			write-host "Brace: reduce incoming damage to a zone on the next turn"
+			write-host "Sail: change the distance between the ships"
+			write-host "Reference: get a quick list of commands to run in case you forget syntax"
+			write-host "Arrows: fire a volley of arrows from distance 2 or closer to deal damage to the enemy crew"
 		}
 		
 		#range 2, similar to a skirmish in damage. does nothing to hull or cannons
@@ -739,6 +823,7 @@ function makeVikingShip() {
 		Export-ModuleMember -Function help
 		Export-ModuleMember -Function reArm
 		Export-ModuleMember -Function dmgArrows
+		Export-ModuleMember -Function reference
     }
     return $ship
 }
@@ -858,7 +943,7 @@ function makeCursedShip() {
 			$dmg[3] = $count / (9+($Dis / 1.5))
 			
 			for ($i = 0; $i -lt 4; $i++) {
-				$dmg[$i] = [math]::Ceiling( (Get-Random(0, ($dmg[$i] * 2))) )
+				$dmg[$i] = [math]::Ceiling( (Get-Random(($dmg[$i] / 2), ($dmg[$i] * 2))) )
 			}
 			
 			return $dmg
@@ -878,7 +963,7 @@ function makeCursedShip() {
 			$dmg[3] = $count / (0.5+($Dis / 1.5))
 			
 			for ($i = 0; $i -lt 4; $i++) {
-				$dmg[$i] = [math]::Ceiling( (Get-Random(0, ($dmg[$i] * 2))) )
+				$dmg[$i] = [math]::Ceiling( (Get-Random(($dmg[$i] / 2), ($dmg[$i] * 2))) )
 			}
 			
 			return $dmg
@@ -898,7 +983,7 @@ function makeCursedShip() {
 			$dmg[3] = $count / (5+($Dis / 1.5))
 			
 			for ($i = 0; $i -lt 4; $i++) {
-				$dmg[$i] = [math]::Ceiling( (Get-Random(0, ($dmg[$i] * 2))) )
+				$dmg[$i] = [math]::Ceiling( (Get-Random(($dmg[$i] / 2), ($dmg[$i] * 2))) )
 			}
 			
 			return $dmg
@@ -924,8 +1009,29 @@ function makeCursedShip() {
 			return $Dis
 		}
 		
+		function reference() {
+			write-host "choose from: 'grape', 'chain', 'round', 'wait', 'board', 'move', 'retreat', 'repair', 'rearm', 'flame', 'brace', 'sail', or 'help'"
+		}
+		
 		function help() {
-			echo "choose from: 'grape', 'chain', 'round', 'wait', 'board', 'move', 'retreat', 'repair', 'rearm', 'flame', 'brace', 'sail', or 'help'"
+			write-host "Cursed Ship:"
+			write-host "Nicknamed the 'lucky_13', the captain of this vessel bewitched the cannons"
+			write-host "It backfired, however, and the cannons have a chance to either deal massive damage or none at all"
+			write-host "Do you feel lucky?"
+			write-host "Actions:"
+			write-host "Grape: fire grapeshot. Damage: --crew, /\cannons, \/hull"
+			write-host "Chain: fire chainshot. Damage: /\crew, \/cannons, --hull"
+			write-host "Round: fire roundshot. Damage: \/crew, /\cannons, --hull"
+			write-host "Wait: skip an action"
+			write-host "Board: board the enemy ship with your crew"
+			write-host "Move: move your crew from one zone to another on a ship"
+			write-host "Retreat: pull back boarders from the opponent's ship"
+			write-host "Repair: order the crew to patch the hull in a zone"
+			write-host "Rearm: order the crew to move cannons from one zone to another"
+			write-host "Flame: order the crew to set fire to a zone"
+			write-host "Brace: reduce incoming damage to a zone on the next turn"
+			write-host "Sail: change the distance between the ships"
+			write-host "Reference: get a quick list of commands to run in case you forget syntax"
 		}
 
         Export-ModuleMember -Variable Name
@@ -948,6 +1054,7 @@ function makeCursedShip() {
 		Export-ModuleMember -Function sail
 		Export-ModuleMember -Function help
 		Export-ModuleMember -Function reArm
+		Export-ModuleMember -Function reference
     }
     return $ship
 }
@@ -1118,8 +1225,30 @@ function makeTurtleShip() {
 			return $Dis
 		} 
 		
+		function reference() {
+			write-host "choose from: 'grape', 'chain', 'round', 'wait', 'board', 'move', 'retreat', 'repair', 'rearm', 'flame', 'brace', 'guard', or 'help'"
+		}
+		
 		function help() {
-			echo "choose from: 'grape', 'chain', 'round', 'wait', 'board', 'move', 'retreat', 'repair', 'rearm', 'flame', 'brace', 'guard', or 'help'"
+			write-host "Turtle Ship:"
+			write-host "The thiccest boat on the seven seas, with a near-impenetrable hull and defensive measures that can ward off any cannon fire"
+			write-host "The designers of the ship did not intend on it moving, however, and it changes course so slowly that it cannot move in combat"
+			write-host "It can defend and fire from the same zone using the 'guard' command though"
+			write-host "The hull is too thick to be patched on the fly, so the repair function fixes up cannons instead"
+			write-host "Actions:"
+			write-host "Grape: fire grapeshot. Damage: --crew, /\cannons, \/hull"
+			write-host "Chain: fire chainshot. Damage: /\crew, \/cannons, --hull"
+			write-host "Round: fire roundshot. Damage: \/crew, /\cannons, --hull"
+			write-host "Wait: skip an action"
+			write-host "Board: board the enemy ship with your crew"
+			write-host "Move: move your crew from one zone to another on a ship"
+			write-host "Retreat: pull back boarders from the opponent's ship"
+			write-host "Repair: order the crew to patch the hull in a zone"
+			write-host "Rearm: order the crew to move cannons from one zone to another"
+			write-host "Flame: order the crew to set fire to a zone"
+			write-host "Brace: reduce incoming damage to a zone on the next turn"
+			write-host "Reference: get a quick list of commands to run in case you forget syntax"
+			write-host "Guard: quickly set up defenses in a zone, so that the crew in that zone can still take another action"
 		}
 
         Export-ModuleMember -Variable Name
@@ -1142,19 +1271,20 @@ function makeTurtleShip() {
 		Export-ModuleMember -Function sail
 		Export-ModuleMember -Function help
 		Export-ModuleMember -Function reArm
+		Export-ModuleMember -Function reference
     }
     return $ship
 }
 
 function readShip($name, $code) {
 	
-	if ($name -eq "Ram" -or $name -eq "hullbuster" -or $name -eq "hull_buster") {
+	if ($name -eq "Ram" -or $name -eq "hullbuster" -or $name -eq "hull buster") {
 		write-host "you have chosen the ramming ship"
 		$code = 1
 		$ship = makeRammingShip
 		$ship.Name = $name
 		return $ship
-	} if ($name -eq "ghost" -or $name -eq "undead" -or $name -eq "flying_dutchman") {
+	} if ($name -eq "ghost" -or $name -eq "undead" -or $name -eq "flying dutchman") {
 		write-host "you have chosen the undead ship"
 		$code = 2
 		$ship = makeUndeadShip
@@ -1166,7 +1296,7 @@ function readShip($name, $code) {
 		$ship = makeVikingShip
 		$ship.Name = $name
 		return $ship
-	} if ($name -eq "cursed" -or $name -eq "lucky_13" -or $name -eq "lucky") {
+	} if ($name -eq "cursed" -or $name -eq "lucky 13" -or $name -eq "lucky") {
 		write-host "you have chosen the 'lucky' ship"
 		$code = 4
 		$ship = makeCursedShip
